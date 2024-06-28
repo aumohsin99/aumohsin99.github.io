356f0ba688fa39dc326da0bddbd73373b9b22574
Ata Ul Mohsin <ataulmohsin1999@gmail.com>
2024-06-28 20:02:11 +0500

    button workaround

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html", "./script.js"],
+  darkMode: "class", // or 'media' if you prefer
   theme: {
     extend: {},
   },
